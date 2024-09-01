30f1e2240670d3d3d239fe94ef3d7fb9b84d3ee5
Vityazzz5830 <121909224+Vityazzz5830@users.noreply.github.com>
2024-09-01 22:16:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,108 @@
+
+/*---------!!! СЛАЙДЕРЫ ИНИЦИАЛИЗАЦИЯ !!!------------------*/
+
+$('.firstsec_content_uppart_list_item_slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: false,
+    infinite: false,
+    dots: false,
+    autoplay: false,
+    variableWidth: true,
+    prevArrow: $('.firstsec_content_uppart_list_item_slider_arrows_leftbtn'),
+    nextArrow: $('.firstsec_content_uppart_list_item_slider_arrows_rightbtn')
+});
+
+$('.firstsec_content_centrepart_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    infinite: true,
+    dots: true,
+    autoplay: true,
+    variableWidth: true
+});
+
+$('.reviewssec_content_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: false,
+    infinite: true,
+    dots: false,
+    autoplay: false,
+    variableWidth: true,
+    prevArrow: $('.reviewssec_content_slider_buttons_leftpart'),
+    nextArrow: $('.reviewssec_content_slider_buttons_rightpart')
+});
+
+
+
+
+window.addEventListener("DOMContentLoaded", function() {
+  [].forEach.call( document.querySelectorAll('input[data-tel-input]'), function(input) {
+    var keyCode;
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = "+7 (___) ___ ____",
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, ""),
+          new_value = matrix.replace(/[_\d]/g, function(a) {
+              return i < val.length ? val.charAt(i++) : a
+          });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+          i < 5 && (i = 3);
+          new_value = new_value.slice(0, i)
+      }
+      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+          function(a) {
+              return "\\d{1," + a.length + "}"
+          }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+        this.value = new_value;
+      }
+      if (event.type == "blur" && this.value.length < 5) {
+        this.value = "";
+      }
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false);
+  });
+});
+
+var nameInputs = document.querySelectorAll('input[data-name-input]');
+
+$(nameInputs).on('keypress', function() {
+    var that = this;
+
+    setTimeout(function() {
+        var res = /[^аА-яЯ]/g.exec(that.value);
+        console.log(res);
+        that.value = that.value.replace(res, '');
+    }, 0);
+});
+
+
+var emailInputs = document.querySelectorAll('input[data-email-input]');
+
+$(emailInputs).on('keypress', function() {
+    var that = this;
+
+    setTimeout(function() {
+        var res = /[^aA-zZ1234567890@.?!%&()><*-_]/g.exec(that.value);
+        console.log(res);
+        that.value = that.value.replace(res, '');
+    }, 0);
+});
+
+
